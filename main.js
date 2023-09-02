6295bd37e817c63d340d31425879f6ff32697062
Santiago Marquinez <abogmarquinez@gmail.com>
2023-09-02 18:23:28 -0300

    elimine los archivos borrador js y html y agregue todas las modificaciones en index y main. cambie algunos estilos con css

--- main.js
+++ main.js
@@ -1,131 +1,149 @@
-//objetos
-
-class NuevoUsuario{
-    constructor(usuario, password){
-        this.usuario = usuario;
-        this.password = password;
-    }
-}
-
-class Articulo{
-    constructor(item, precio, stock, foto){
-    this.item = item;
-    this.precio = precio;
-    this.stock = stock;
-    this.foto = foto;    
-    }
-}
-
-class ArticuloVendido{
-    constructor(item, precio, foto){
+//funciones
+class Articulo {
+    constructor(item, precio, stock, foto) {
         this.item = item;
         this.precio = precio;
+        this.stock = stock;
         this.foto = foto;
     }
 }
 
+// Función para vaciar el carrito
+function vaciarCarrito(carrito) {
+    carrito.length = 0; // Esto elimina todos los elementos del arreglo
+    localStorage.removeItem("carrito"); // También puedes quitar el carrito del almacenamiento local si lo deseas
+    cart(carrito); // Llama nuevamente a la función cart para actualizar la vista del carrito
+}
 
-//Funciones
-const mayor = (edad,admitir)=>{
-    if (edad>=18){
-        return true;
+function eliminarDelCarrito(carrito, index) {
+    carrito.splice(index, 1); // Elimina el producto del carrito en el índice especificado
+    localStorage.setItem("carrito", JSON.stringify(carrito)); // Actualiza el carrito en el almacenamiento local
+    cart(carrito); // Vuelve a renderizar el carrito para reflejar los cambios
+}
+
+function cart(carrito) {
+    const divBotones = document.createElement("div");
+    divBotones.classList.add("botones-carrito");
+    if (carrito.length > 0) {
+        contenedorProductos.innerHTML = "";
+        carrito.forEach((producto, index) => {
+            const div = document.createElement("div");
+            div.classList.add("tarjeta");
+            div.innerHTML = `
+            <img src="${producto.foto}" alt="${producto.item}" class="card-img-top imagen">
+            <div class="card-body">
+            <h5 class="card-title">${producto.item}</h5>
+            <h6 class="card-text">$${producto.precio}</h6>
+            <button class="btn btn-danger eliminar" data-index="${index}">Eliminar</button>
+            </div>
+            `;
+    
+            const botonEliminar = div.querySelector(".eliminar");
+            botonEliminar.addEventListener("click", () => eliminarDelCarrito(carrito, index));
+    
+            contenedorProductos.appendChild(div);
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+        });
+        } else{
+            contenedorProductos.innerHTML = `
+            <h2 class="encabezado">Lo siento, no hay productos en el carrito</h2>
+            `;
+        }
+    const montoCompra = document.createElement("p");
+    montoCompra.classList.add("parrafoMonto");
+    montoCompra.innerHTML = `Monto de la compra: $0`; // Inicialmente, el monto es cero
+    divBotones.appendChild(montoCompra);
+
+    function calcularMontoCompra() {
+        const total = carrito.reduce((acc, producto) => acc + producto.precio, 0);
+        montoCompra.innerHTML = `Monto de la compra: $${total}`;
+    };
+
+    calcularMontoCompra();
+    
+    const botonPagar = document.createElement("button");
+    botonPagar.textContent = "Pagar";
+    botonPagar.classList.add("btn", "btn-success", "botonPagar");
+    divBotones.appendChild(botonPagar);
+
+    const botonVaciarCarrito = document.createElement("button");
+    botonVaciarCarrito.textContent = "Vaciar Carrito";
+    botonVaciarCarrito.classList.add("btn", "botonVaciar", "botonVaciarCarrito");
+    divBotones.appendChild(botonVaciarCarrito);    
+    botonVaciarCarrito.addEventListener("click", () => vaciarCarrito(carrito));
+    contenedorProductos.appendChild(divBotones);        
+}
+
+function agregarAlCarrito(carrito, producto) {
+    carrito.push(producto);
+}
+
+// Calcula y muestra el monto de la compra
+function calcularMontoCompra() {
+    const total = carrito.reduce((acc, producto) => acc + producto.precio, 0);
+    montoCompra.innerHTML = `Monto de la compra: $${total}`;
+}
+
+function cargarProductos(productos) {
+    const carritoGuardado = JSON.parse(localStorage.getItem("carrito"))
+    const carrito = [];
+    if(!carritoGuardado){
+        const carrito = [];
     }else{
-        return false;
-    }
-}
-
-function masCaro (carrito){
-    let max = 0;
-    let caro = "";
-    for (i=0; i<carrito.length; i++){
-        if(carrito[i].precio > max){
-            max = carrito[i].precio;
-            caro = carrito[i].item;        
+        for(i=0; i<carritoGuardado.length; i++){
+            carrito.push(carritoGuardado[i]);
         }
     }
-    alert (`El artículo más caro es: ${caro}`);
-}
+    contenedorProductos.innerHTML = "";
 
-function sumar(prod, carrito){
-    for (const item of carrito){
-        if (item.item === prod){
-            item.ventas++;
-            break;
-        } 
-    }
-}
+    productos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("tarjeta");
+        div.innerHTML = `
+        <img src="${producto.foto}" alt="${producto.item}" class="card-img-top imagen">
+        <div class="card-body">
+        <h5 class="card-title">${producto.item}</h5>
+        <h6 class="card-text">$${producto.precio}</h6>
+        <button name="button" class="btn btn-primary boton">Agregar al carrito</button>
+    
+        </div>
+        `;
 
+        const botonAgregar = div.querySelector(".boton");
+        botonAgregar.addEventListener("click", () => agregarAlCarrito(carrito, producto));
 
-const comprar = (carrito) => {
-    carrito = [];
-    const artVendido = [];
-    let cantidad = 0;
-    let monto = 0;
-    alert("usted puede compar camiseta, short o campera");
-    let articulo = prompt("¿Que desea comprar?");
-    while (articulo != "pagar") {
-        switch (articulo) {
-            case "camiseta":
-                carrito.push(new ArticuloVendido ("camiseta", 15000,"camiseta.png"));
-                cantidad++;
-                monto +=15000;
-                sumar ("camiseta",carrito);
-                break;
-            case "short":
-                carrito.push(new ArticuloVendido ("short", 10000,"short.png"));
-                cantidad++;
-                monto +=15000;
-                sumar ("short",carrito);
-                break;
-            case "campera":
-                carrito.push(new ArticuloVendido ("campera", 30000,"campera.png"));
-                cantidad++;
-                monto +=15000;
-                sumar ("campera",carrito);
-                break;
-            default:
-                alert("no ingreso un artículo valido")
-                break;
-        }
-        articulo = prompt("Para seguir comprando ingrese otro artículo. Si ya terminó ingrese `pagar`");
-    }
-    if(monto != 0){
-        let carritoInfo = "";
+        contenedorProductos.append(div);
+    });
+    const contBotonCarrito = document.createElement("div");
+    contBotonCarrito.innerHTML =`
+    <button name="button" class="btn btn-primary boton botonCarrito">Carrito</button>
+    `;
+    contenedorProductos.append(contBotonCarrito);
+    contBotonCarrito.addEventListener("click", () => cart(carrito));
 
-        for (const item of carrito){
-            carritoInfo += `${item.item}, ${item.precio}\n`;
-        }
-        alert(`Usted tiene en el carrito los siguientes productos: \n ${carritoInfo} `);
-        alert(`Usted compró ${cantidad} artículo/s. El total de su factura es $ ${monto}. Gracias por su compra`);
-    }else {
-        alert("Gracias por visitar nuestra tienda");
-    }
-    masCaro(carrito);
 }
 
 // Algoritmo principal
-
-let header = document.querySelector("header");
+const header = document.querySelector("header");
 let h1 = document.createElement("h1");
-h1.textContent = "Bienvenido a la tienda";
+h1.innerHTML = "Bienvenido a la tienda";
 header.appendChild(h1);
 h1.classList.add("encabezado");
 
 document.body.className = "fondo";
-// const body= document.querySelector(".fondo");
-// body.classList.add ("probando")
+
+const contenedorProductos = document.querySelector(".vidriera");
 const productos = [];
-productos.push(new Articulo ("camiseta", 15000, 30, "./images/camiseta.webp" ));
-productos.push(new Articulo ("short", 10000, 43, "./images/short.webp" ));
-productos.push(new Articulo ("campera", 30000, 15, "./images/campera.png" ));
-let admitir = false;
-let edad = parseInt(prompt("Ingrese su edad"))
-const carrito = [];
-const respuesta = mayor(edad, admitir);
-console.log(respuesta)
-if (respuesta) {
-    alert("Bienvenido a la tienda");
-    comprar(carrito);
-} else {
-    alert("Usted no tiene edad para acceder a esta tienda");
-}
+productos.push(new Articulo("camiseta", 15000, 30, "./images/camiseta.webp"));
+productos.push(new Articulo("short", 10000, 43, "./images/short.webp"));
+productos.push(new Articulo("campera", 30000, 15, "./images/campera.png"));
+cargarProductos(productos);
+
+
+
+
+
+
+
+
+
